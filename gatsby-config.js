d15aacbd24c40d5706223b1b11c648f68c713583
addwis <addwis.jp@gmail.com>
2020-04-28 23:42:36 +0900

    Add: wordpress側で追加したtaxonomyを反映する処理を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,39 @@ module.exports = {
         auth: {},
         // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: false,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+          "**/pickups",
+          "**/blocks",
+          "**/wp_v2",
+          "**/comments",
+          "**/search",
+          "**/statuses",
+          "**/types"
+        ],
+        // use a custom normalizer which is applied after the built-in ones.
+        normalizer: function ({ entities }) {
+          const pickups = entities.filter(e => e.__type === 'wordpress__wp_pickups')
+          return entities.map(e => {
+            if (e.__type === 'wordpress__POST') {
+              let hasPickups = e.pickups && Array.isArray(e.pickups) && e.pickups.length
+              // Replace pickups with links to their nodes.
+              if (hasPickups) {
+                e.pickups___NODE = e.pickups.map(
+                  c => pickups.find(pObj => c === pObj.wordpress_id).id
+                )
+              }
+              delete e.pickups
+            }
+            return e
+          })
+        },
       },
     },
     'gatsby-plugin-sharp',
